703afaebfab0f2e6d8b0adab7caa51ee1e8f82d6
Trevor <68875146+tcode-g@users.noreply.github.com>
2021-11-19 13:58:26 -0500

    tidy

--- schedule_2.js
+++ schedule_2.js
@@ -1,5 +1,5 @@
 const scheduleTimes = {
-  "Regular" : [
+  "Regular": [
     ["7:15", "8:03"],
     ["8:10", "8:58"],
     ["8:58", "9:09", "Break"],
@@ -7,59 +7,55 @@ const scheduleTimes = {
     ["10:17", "11:05"],
     ["11:05", "11:35", "Lunch"]
   ],
-  "Wednesday" : [
+  "Wednesday": [
     ["7:15", "7:56"],
     ["8:03", "8:44"],
-//  ["", "", "Break"],
+    //  ["", "", "Break"],
     ["8:51", "9:40"],
     ["9:47", "10:28"],
     ["10:28", "11:00", "Lunch"]
   ]
 }
-    
 
-var [day, hrs, min] = getDateVars()
+
+var [day, hrs, min, ms] = getDateVars()
 
 var schoolDay = (day > 0 && day < 6)
 var schoolHrs = (day == 3) ? scheduleTimes.Wednesday : scheduleTimes.Regular
 
 let startTime = new Date()
-startTime.setHours(0,0,0,0)
-console.log(new Date(startTime.getTime()).toLocaleTimeString())
+startTime.setHours(0, 0, 0, 0)
 
 
 function getDateVars() {
   let d = new Date()
-  return [d.getDay(), d.getHours(), d.getMinutes()]
+  return [d.getDay(), d.getHours(), d.getMinutes(), d.getTime()]
 }
 function getNextClass() {
-  
+
   for (let i = 0; i < schoolHrs.length; i++) {
+
+    [day, hrs, min, ms] = getDateVars()
+
     let classTimes = schoolHrs[i]
     let start = classTimes[0]
     let end = classTimes[1]
-    
 
-    
-    if(classTimes.length == 3 && classTimes[2] == "Break") {
+    if (classTimes.length == 3 && classTimes[2] == "Break") {
       console.log("break")
-
+      return i;
     } else {
       console.log("not break")
     }
-    
+
     let startSplit = start.split(":")
     let endSplit = end.split(":")
-    
-    let startOfClass = ((parseInt(startSplit[0])*60)+parseInt(startSplit[1]))*1000
-    let endOfClass
-=((parseInt(endSplit[0])*60)+parseInt(endSplit[1]))*1000
-    console.log(startTime.getTime()+endOfClass)
-    
-//     
-//     if (startSplit[0] == hrs.toString()) {
-//       
-//     }  
+    let F = (tbl) => {return ((parseInt(tbl[0]) * 60) + parseInt(tbl[1])) * 60 * 1000}
+    let startOfClass = F(startSplit)
+    let endOfClass = F(endSplit)
+    if (ms < startOfClass && ms < endOfClass) {
+      return i;
+    }
   }
 }
 
