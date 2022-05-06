db61e1c11c1a2ae40958b8bace1b7268e284f22c
Trevor <68875146+tcode-g@users.noreply.github.com>
2022-05-05 21:57:21 -0400

    removed setTriggerDate for now, seems to be the error

--- future/test_timer.js
+++ future/test_timer.js
@@ -5,7 +5,5 @@ noti.body = "This is some test text!"
 
 // 10 seconds after 
 Timer.schedule(10 * 1000, false, () => {
-    noti.setTriggerDate(new Date())
     noti.schedule()
-    log("noti should've fired")
 })
