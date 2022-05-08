c49b2e0dbe3b2fc2ef4615be18ad4333ea23a70b
Trevor <68875146+tcode-g@users.noreply.github.com>
2022-05-08 00:58:24 -0400

    removing the notification test

--- schedule_pseudoc.js
+++ schedule_pseudoc.js
@@ -1,12 +1,4 @@
-const noti = new Notification()
-noti.title = "Yo"
-noti.subtitle = "Sub"
-noti.body = "This is some test text!"
 
-// 10 seconds after 
-Timer.schedule(10 * 1000, false, () => {
-    noti.schedule()
-})
 
 
 /* pseudocode for code renovation
